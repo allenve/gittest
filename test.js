85c948c418b589719da49869932b76a4a058bae9
yupeng12 <yupeng12@baidu.com>
2019-04-18 17:15:24 +0800

    aa

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 const message = "this is a message from master";
 const message2 = "add a message again";
 const branch_message = "this a message from branch test";
+cosnt message3 = "add by master "
