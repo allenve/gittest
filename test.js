b3151d5422155e90fb9646ac6351bec19ef3ae90
yupeng12 <yupeng12@baidu.com>
2019-04-18 18:48:01 +0800

    brach test 1commit

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
 const message = "this is a message from master";
-
-const branch_message = ""
+const message2 = "add a message again";
+const branch_message = "this a delete by test1 brancht";
+const message3 = "add by master ..."
+let a = "rebase test"
